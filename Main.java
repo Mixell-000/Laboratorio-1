552a7cb47f14d47cbe89a627fbc2186edd0b96f9
Javier Alonso Santibáñez Carrasco <javier.santibanez1@mail.udp.cl>
2025-03-27 15:40:39 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    char A = 'A';
+	    char B = 'B';
+	    char a = 'a';
+	    int potA = A;
+	    int potB = B;
+	    int pota = a;
+	    
+	    
+		//System.out.print(potA - 65 + " (" + potA + ")");
+		//System.out.print(" ");
+		//System.out.print(potB - 65);
+		//System.out.print(" ");
+		//System.out.print(pota - 97);
+		
+		
+		Scanner Poto = new Scanner(System.in);
+	    System.out.println("dime una letra");
+	    
+	    String Letra = Poto.nextLine();
+	    
+	    for(int i = 0; i < Letra.length(); i++){
+	        int xD = Letra.charAt(i);
+	    
+	    if(xD > 91){
+	        System.out.println(xD - 97 + "<- Se va pa la matriz de minusculas");
+	    }else{
+	        System.out.println(xD - 65 + "<- Se va pa la matriz de Mayusculas");
+	    }
+	    }
+	    
+        
+	}
+}
