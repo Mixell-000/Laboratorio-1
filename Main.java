840459ff89d1570b97b9e5c8e2ce696484768bbd
Javier Alonso Santibáñez Carrasco <javier.santibanez1@mail.udp.cl>
2025-04-08 16:26:18 -0400

    Create Main.java
    Creé el main, holaa

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+		//Aquí vamos poniendo nuestro codigo principal a partir de los codigos secundarios que hemos hecho.
+
+public class Main
+{
+	public static void main(String[] args) {
+
+
+
+
+    
+  }
+}
