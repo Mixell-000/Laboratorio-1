1818ab2ea8c99d24bc76a55b623869e7794e80cb
Javier Alonso Santibáñez Carrasco <javier.santibanez1@mail.udp.cl>
2025-04-08 16:34:45 -0400

    Update Main.java
    Agregué la funcion Creadora de matriz

--- Main.java
+++ Main.java
@@ -4,11 +4,40 @@ import java.io.*;
 
 public class Main
 {
+	public static char[][] CreadorDeMatriz(){
+		
+		String VocabuPapu1 = "⊡abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; // la cantidad de caracteres puede cambiar, de nada papus (-> VocabuPapu.length())
+		String VocabuPapu = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; 
+
+		char[] arr =new char [VocabuPapu.length()];
+		
+		char[][] PAPUGENERADOR = new char [VocabuPapu1.length()] [VocabuPapu1.length()];
+		
+		for(int i = 0; i < VocabuPapu.length();i++){ //En este for se chanta todo VocabuPapu en el array "arr" xD
+			arr[i] = VocabuPapu.charAt(i);
+			}
+
+        for(int i = 0; i < VocabuPapu.length(); i++){
+            PAPUGENERADOR[i][0] = VocabuPapu1.charAt(i); //Es una fila con diferentes caracteres porque está el cuadradito
+        }
+        
+		for(int a = 0; a < VocabuPapu.length()-1; a++){ //a en este caso es el numero de la linea en la que están~
+		    for(int i = 0; i<VocabuPapu.length(); i++){ //esta cagada recorre la fila de derecha a izquierda
+		                
+		                    
+		                        PAPUGENERADOR[i][a+1] = arr[(i+a) % VocabuPapu.length()];
+            }                   // i = fila, a = columna
+
+		}
+
+		return PAPUGENERADOR;
+		
+	
+	}
+	
 	public static void main(String[] args) {
-
-
-
-
     
   }
+
+	
 }
