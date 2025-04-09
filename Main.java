7e5f770412e146b876fe004cd84708e625c7c898
Javier Alonso Santibáñez Carrasco <javier.santibanez1@mail.udp.cl>
2025-04-09 12:14:04 -0400

    Update Main.java
    Agregué un array que convierte un string en sus coordenadas y de paso tira los espacios a >1000 para que lo actualicemos luego en el encriptador / desencriptador

--- Main.java
+++ Main.java
@@ -2,6 +2,46 @@ import java.util.*;
 import java.io.*;
 		//Aquí vamos poniendo nuestro codigo principal a partir de los codigos secundarios que hemos hecho.
 
+
+
+    public static int Convertidor(string x){ //string x es lo que recibimos
+	    
+	    int[] entrega = new int [x.length()];
+	    
+	    for(int i = 0; i < x.length(); i++){
+	
+	    if(x.charAt(i) == 241){
+	        
+	        entrega[i] = x.charAt(i)- 177;
+	        
+	    }else if(x.charAt(i) == 209){
+	        
+	        entrega[i] = x.charAt(i)- 146;
+	        
+	    }else if(x.charAt(i) > 91){ //en caso de minusculas
+	    
+	        entrega[i] = x.charAt(i)- 96;
+	        
+	    }else if(x.charAt(i) > 64){//en caso de mayusculas
+	    
+	        entrega[i] = x.charAt(i)- 38;
+	        
+	    }else if(x.charAt(i) > 47){
+	        
+	        entrega[i] = x.charAt(i) + 5;
+	        
+	    }else if(x.charAt(i) == 32){
+	        entrega[i] = x.charAt(i) + 1000;
+
+	    }
+	    }
+	    
+        	    for(int i = 0; i < x.length(); i++){
+        	    
+        	        System.out.println(entrega[i]);
+        	    }
+
+	    }
 public class Main
 {
 	public static char[][] CreadorDeMatriz(){
